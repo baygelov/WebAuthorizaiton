c8a4da7bd8f98fc05e6b69cc33c68a501e7014d1
Александр Байгелов <baygelov2000@gmail.com>
2022-09-06 20:41:22 +0300

    message

---
+++ Authorization/Areas/Identity/Data/AuthContext.cs
@@ -0,0 +1,30 @@
+﻿using Authorization.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Authorization.Data;
+
+public class AuthContext : IdentityDbContext<User>
+{
+    public AuthContext(DbContextOptions<AuthContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.Email).HasMaxLength(255);
+        builder.Property(u => u.Password).HasMaxLength(255);
+    }
+}
